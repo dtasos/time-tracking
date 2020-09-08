9ccd5f3d6f8a32593959746925f406b55389ff83
dtasos <dimitriou.tas@gmail.com>
2020-09-08 14:14:22 +0300

    page114

--- components/TimerForm.js
+++ components/TimerForm.js
@@ -36,4 +36,37 @@ export default function TimerForm({ id, title, project}) {
     )
 }
 
-const styles = StyleSheet.create
+const styles = StyleSheet.create({
+    formContainer: {
+        backgroundColor: 'white',
+        borderColor: '#D6D7DA',
+        borderWidth: 2,
+        borderRadius: 10,
+        padding: 15,
+        margin: 15,
+        marginBottom: 0,
+    },
+    attributeContainer: {
+        marginVertical: 8,
+    },
+    textInputContainer: {
+        borderColor: '#D6D7DA',
+        borderRadius: 2,
+        borderWidth: 1,
+        marginBottom: 5,
+    },
+    textInput: {
+        height: 30,
+        padding: 5,
+        fontSize: 12
+    },
+    textInputTitle: {
+        fontSize: 14,
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
+    buttonGroup: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    }
+})
